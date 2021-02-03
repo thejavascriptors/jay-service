0111225d382a49c60eb3cafcde9d407629335630
Jason Panay <jasonpanay@gmail.com>
2021-02-03 16:07:41 -0500

    Got basic API call hooked up with PostgreSQL and added notes for further goals

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,15 @@ const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
 let faker = require('faker');
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  password: 'root',
+  database: 'sdc',
+  host: 'localhost',
+  port: 5432
+});
 
 
 const app = express();
@@ -13,9 +22,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', (req, res) => {
-  // let id = req.params.id;
-  res.send(200);
+app.get('/api/:id', (req, res) => {
+  // Queries should execute in less than 50ms
+  // Reach 1k rps on machine / 100 on ec2
+  // API response time leess than 2000ms
+  // Max error rate 1%
+  // Stress test Httperf, Jmeter, Artillery,
+  let id = req.params.id;
+
+  let queryOne = `SELECT * FROM products WHERE id = '${id}'`;
+  pool.query(queryOne, (err, response) => {
+    console.log('GETTING');
+    err ? res.send(err) : res.send(response);
+  });
+
 });
 
 
