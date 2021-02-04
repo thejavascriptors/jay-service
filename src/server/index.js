f6897df50bb8f2130951c07e7aaac82c6b0aaef4
Jason Panay <jasonpanay@gmail.com>
2021-02-04 03:26:39 -0500

    API sending correct data shape (need to improve 3870 ms res time tho)

--- src/server/index.js
+++ src/server/index.js
@@ -29,13 +29,75 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/products/:id', (req, res) => {
   let id = req.params.id;
   // Ask about inner joins vs double inner joins vs regular queries for creating structure
-  let queryOne = `SELECT * FROM products INNER JOIN features USING(product_id) INNER JOIN images USING(product_id) WHERE products.product_id = '${id}'`;
-  pool.query(queryOne, (err, response) => {
-    console.log('Querying...');
-    err ? res.send(err) : res.send(response);
+  // let multiQuery = `SELECT * FROM products INNER JOIN features USING(product_id) INNER JOIN images USING(product_id) WHERE products.product_id = '${id}'`;
+  // pool.query(multiQuery, (err, response) => {
+  //   console.log('Querying...');
+  //   err ? res.send(err) : res.send(response);
+  // });
+
+  let product;
+  // Single query
+  let productQuery = `SELECT * FROM products WHERE product_id = '${id}'`;
+  let featureQuery = `SELECT * FROM features WHERE product_id = '${id}'`;
+  let imageQuery = `SELECT * FROM images WHERE product_id = '${id}'`;
+  pool.query(productQuery, (err, pResRows) => {
+    if (err) { return res.send(err); }
+    let pRes = pResRows.rows[0];
+    console.log(pRes);
+    product = {
+      name: pRes.title,
+      brand: pRes.brand,
+      stars: pRes.stars,
+      ratings: pRes.ratings,
+      shorthand: pRes.shorthand,
+      price: pRes.price,
+      stock: pRes.stock,
+      shipping: { date: pRes.shipdate, supplier: pRes.shipsupplier },
+      description: 'pRes.descriptions' // Need to add
+    };
+    pool.query(featureQuery, (err, fRes) => {
+      if (err) { return res.send(err); }
+      let featureRows = fRes.rows;
+      product.features = [];
+      for (let featureObj of featureRows) {
+        product.features.push(featureObj.feature);
+      }
+      pool.query(imageQuery, (err, iRes) => {
+        if (err) { return res.send(err); }
+        let imageRows = iRes.rows;
+        product.photos = [];
+        for (let imageObj of imageRows) {
+          let photo = {};
+          photo.url = imageObj.imageurl;
+          photo.description = imageObj.descriptions;
+          product.photos.push(photo);
+        }
+        res.send(product);
+      });
+    });
   });
 });
 
+/*
+  let dataTemplate = {
+    name: '',
+    brand: '',
+    stars: '',
+    ratings: '',
+    shorthand: '',
+    price: '',
+    stock: '',
+    shipping: { date: '', supplier: '' },
+    features: [ '', '', '' ],
+    description: '',
+    photos: [
+      { url: '', description: '' },
+      { url: '', description: '' },
+      { url: '', description: '' }
+    ]
+  };
+});
+*/
 
 const PORT = 3000;
 app.listen(PORT, () => console.log('Server is running on port', PORT));
