8db59e3624cd36ae8a82a1b8f46d294530a6aed6
Jason Panay <jasonpanay@gmail.com>
2021-02-06 10:22:32 -0500

    MongoDB routes set up

--- src/server/index.js
+++ src/server/index.js
@@ -5,8 +5,19 @@ const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
 let faker = require('faker');
+const mongoose = require('mongoose');
+const Product = require('./mongoDB');
+const secret = require('./secret');
 const { Pool, Client } = require('pg');
 
+// Mongo
+let mongoAtlas = secret.mongoURI;
+let mongoLocal = 'mongodb://127.0.0.1:27017/sdclocal';
+mongoose.connect(mongoAtlas, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => { console.log('---------- Mongo Connected ----------'); });
+// PostgreSQL
 const pool = new Pool({
   user: 'postgres',
   password: 'root',
@@ -22,47 +33,57 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+// MongoDB
 app.get('/products/:id', (req, res) => {
   let id = req.params.id;
-  let productQuery = `SELECT * FROM products WHERE product_id = '${id}'`;
-  let featureQuery = `SELECT * FROM features WHERE product_id = '${id}'`;
-  let imageQuery = `SELECT * FROM images WHERE product_id = '${id}'`;
-  pool.query(productQuery, (err, pResRows) => {
-    if (err) { return res.send(err); }
-    let pRes = pResRows.rows[0];
-    product = {
-      name: pRes.title,
-      brand: pRes.brand,
-      stars: pRes.stars,
-      ratings: pRes.ratings,
-      shorthand: pRes.shorthand,
-      price: pRes.price,
-      stock: pRes.stock,
-      shipping: { date: pRes.shipdate, supplier: pRes.shipsupplier },
-      description: 'pRes.descriptions' // Need to add to schema / seeding func
-    };
-    pool.query(featureQuery, (err, fRes) => {
-      if (err) { return res.send(err); }
-      let featureRows = fRes.rows;
-      product.features = [];
-      for (let featureObj of featureRows) {
-        product.features.push(featureObj.feature);
-      }
-      pool.query(imageQuery, (err, iRes) => {
-        if (err) { return res.send(err); }
-        let imageRows = iRes.rows;
-        product.photos = [];
-        for (let imageObj of imageRows) {
-          let photo = {};
-          photo.url = imageObj.imageurl;
-          photo.description = imageObj.descriptions;
-          product.photos.push(photo);
-        }
-        res.send(product);
-      });
-    });
-  });
+  Product.findOne({ _id: id })
+    .then((data) => { res.status(200).send(data); })
+    .catch((err) => { res.status(404).send(err); });
 });
+
+
+// PostgreSQL
+// app.get('/products/:id', (req, res) => {
+//   let id = req.params.id;
+//   let productQuery = `SELECT * FROM products WHERE product_id = '${id}'`;
+//   let featureQuery = `SELECT * FROM features WHERE product_id = '${id}'`;
+//   let imageQuery = `SELECT * FROM images WHERE product_id = '${id}'`;
+//   pool.query(productQuery, (err, pResRows) => {
+//     if (err) { return res.status(404).send(err); }
+//     let pRes = pResRows.rows[0];
+//     product = {
+//       name: pRes.title,
+//       brand: pRes.brand,
+//       stars: pRes.stars,
+//       ratings: pRes.ratings,
+//       shorthand: pRes.shorthand,
+//       price: pRes.price,
+//       stock: pRes.stock,
+//       shipping: { date: pRes.shipdate, supplier: pRes.shipsupplier },
+//       description: 'pRes.descriptions' // Need to add to schema / seeding func
+//     };
+//     pool.query(featureQuery, (err, fRes) => {
+//       if (err) { return res.status(404).send(err); }
+//       let featureRows = fRes.rows;
+//       product.features = [];
+//       for (let featureObj of featureRows) {
+//         product.features.push(featureObj.feature);
+//       }
+//       pool.query(imageQuery, (err, iRes) => {
+//         if (err) { return res.status(404).send(err); }
+//         let imageRows = iRes.rows;
+//         product.photos = [];
+//         for (let imageObj of imageRows) {
+//           let photo = {};
+//           photo.url = imageObj.imageurl;
+//           photo.description = imageObj.descriptions;
+//           product.photos.push(photo);
+//         }
+//         res.status(200).send(product);
+//       });
+//     });
+//   });
+// });
 
 
 const PORT = 3000;
